2c8b5e1ec2a104a458f7d0349d738afee39461b0
Tatiana-Futarnaya <tannia93@mail.ru>
2023-04-11 15:23:03 +0300

    second commit

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,6 +1,6 @@
 package com.epam.demo;
 
-import org.apache.commons.lang3.math.NumberUtils;
+import com.epam.utils.StringUtils;
 
 import java.util.List;
 
@@ -8,6 +8,6 @@ import java.util.List;
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
        return args.stream()
-               .allMatch(arg -> NumberUtils.isCreatable(arg) && NumberUtils.toDouble(arg)>0);
+               .allMatch(StringUtils::isPositiveNumber);
     }
 }
